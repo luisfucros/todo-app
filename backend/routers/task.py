0d9e40b5e31b0617fbbd50c2e98ce90e2a0a9dfd
Luis Felipe Salazar Ucros <40307832+luisfucros@users.noreply.github.com>
2025-01-04 23:49:40 +0000

    add update task endpoint

--- backend/routers/task.py
+++ backend/routers/task.py
@@ -31,3 +31,28 @@ def create_task(
     db.commit()
     db.refresh(db_task)
     return db_task
+
+@router.put("/{id}", response_model=schemas.TaskOut)
+def update_task(
+    id: int,
+    updated_task: schemas.TaskCreate,
+    db: Session = Depends(get_db),
+    current_user: schemas.UserOut = Depends(oauth2.get_current_user)
+):
+    task_query = db.query(models.Task).filter(models.Task.id == id)
+
+    task = task_query.first()
+
+    if task is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"task with id: {id} does not exist")
+
+    if task.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform requested action")
+
+    task_query.update(updated_task.model_dump(), synchronize_session=False)
+
+    db.commit()
+
+    return task_query.first()
