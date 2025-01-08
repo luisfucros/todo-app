a7e0554cdebf57e7ef4b5eda415c6e9683af2740
Luis Felipe Salazar Ucros <40307832+luisfucros@users.noreply.github.com>
2025-01-08 00:55:38 +0000

    update file

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,7 @@
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
@@ -11,7 +14,7 @@ class Settings(BaseSettings):
     algorithm: str
     access_token_expire_minutes: int
 
-    class Config:
+    class ConfigDict:
         env_file = ".env"
 
 
--- backend/schemas.py
+++ backend/schemas.py
@@ -34,7 +34,7 @@ class TaskOut(BaseModel):
     description: str
     created_at: datetime
 
-    class Config:
+    class ConfigDict:
         from_attributes = True
 
 
