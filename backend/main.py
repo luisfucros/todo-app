03d317bef8041f434b0e7101ef3d0f0c3208ef2d
Luis Felipe Salazar Ucros <40307832+luisfucros@users.noreply.github.com>
2025-01-09 01:06:03 +0000

    add rate limit

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
 from backend.routers import user, auth, task
 from backend import models
 from backend.database import engine
-import os
+from backend.rate_limit import lifespan
+from fastapi_limiter.depends import RateLimiter
+
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan, dependencies=[Depends(RateLimiter(times=2, seconds=5))])
 
 origins = ["*"]
 
